50c633a57219bfbbf5280f56e0b89623849051b0
Saurav <sauravkumar00108@gmail.com>
2023-09-16 00:38:50 +0530

    change in backend

--- src/routes/recipees.js
+++ src/routes/recipees.js
@@ -1,64 +1,82 @@
-import {
-    RecipeModel
-} from "../models/recipees.js";
+import { RecipeModel } from "../models/recipees.js";
 import { UserModel } from "../models/users.js";
 import express from "express";
 
 const router = express.Router();
+router.post("/", async (req, res) => {
+  const recipe = new RecipeModel(req.body);
+  try {
+    const response = await recipe.save();
+    res.json({ message: "success" });
+  } catch (error) {
+    return res.json({ message: "error" });
+  }
+});
+
 router.get("/", async (req, res) => {
-    try {
-        const response = await RecipeModel.find({});
-        res.json(response);
-    } catch (error) {
-        return res.json(error);
-    }
+  try {
+    const response = await RecipeModel.find({});
+    res.json(response);
+  } catch (error) {
+    return res.json(error);
+  }
+});
+router.get("/:id", async (req, res) => {
+  try {
+    const response = await RecipeModel.findById(req.params.id);
+    res.json(response);
+  } catch (error) {
+    return res.json(error);
+  }
 });
 
-router.put("/",async (req, res) => {
-     
-    try {
-    const recipe=await RecipeModel.findById(req.body.recipeID);
-    const user=await UserModel.findById(req.body.userID);
-    
-    user.savedRecipe.push(recipe); 
-    await user.save();
-    res.json({savedRecipe:user.savedRecipe})
-    } catch (error) {
-        return res.json(error);
-    }
+router.put("/:userId", async (req, res) => {
+  try {
+    const id = req.params.userId;
+    console.log(req.body)
+    const recipe = await RecipeModel.findById(req.body.recipeID);
+    const user = await UserModel.findByIdAndUpdate(id , {$push : {savedRecipe:recipe} }, {new:true});
+
+    res.json(user);
+  } catch (error) {
+    return res.json(error);
+  }
 });
 
-router.post("/createRecipe", async (req, res) => {
-    const recipe = new RecipeModel(req.body);
-    try {
-       const response= await recipe.save();
-       res.json({message:"success"});
-    } catch (error) {
-        return res.json({message:"error"});
-    }
-});
 
-router.get("/savedRecipe/ids/:userID" ,async(req,res)=>{
- try {
-    const user=await UserModel.findById(req.params.userID);
-    res.json({savedRecipe:user?.savedRecipe});
- } catch (error) {
+
+// router.get("/savedRecipe/ids/:userID", async (req, res) => {
+//   try {
+//     const user = await UserModel.findById(req.params.userID);
+//     res.json({ savedRecipe: user?.savedRecipe });
+//   } catch (error) {
+//     res.json(error);
+//   }
+// });
+
+router.get("/savedRecipe/:userID", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userID);
+    const savedRecipe = await RecipeModel.find({
+      _id: { $in: user.savedRecipe },
+    });
+    res.json(savedRecipe);
+  } catch (error) {
     res.json(error);
- }
+  }
 });
 
-router.get("/savedRecipe/:userID" ,async(req,res)=>{
-    try {
-       const user=await UserModel.findById(req.params.userID);
-      const savedRecipe=await RecipeModel.find({
-        _id: {$in: user.savedRecipe}
-      });
-      res.json(savedRecipe);
-      
-    } catch (error) {
-       res.json(error);
-    }
-   });
-export {
-    router as recipeRouter
-};
+router.put("/remove/:userId", async (req, res) => {
+  try {
+    const id = req.params.userId;
+    const recipe = await RecipeModel.findById(req.body.recipeID);
+    const user = await UserModel.findByIdAndUpdate(id , {$pull : {savedRecipe:req.body.recipeID} }, {new:true});
+
+    res.json(user);
+  } catch (error) {
+    return res.json(error);
+  }
+});
+
+
+export { router as recipeRouter };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,71 +1,61 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import bcrypt from 'bcrypt';
-import {
-  UserModel
-} from "../models/users.js";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/users.js";
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-  const {
-    name,
-    username,
-    password
-  } = req.body;
+router.post("/register", async (req, res) => {
+  const { name, username, password } = req.body;
   const user = await UserModel.findOne({
-    username
+    username,
   });
   if (user) {
     return res.json({
-      message: "User Already Exist!"
+      message: "User Already Exist!",
     });
   }
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new UserModel({
     name,
     username,
-    password: hashedPassword
+    password: hashedPassword,
   });
   await newUser.save();
   res.json({
-    message: "user registered  !"
+    message: "user registered  !",
   });
 });
 
-router.post('/login', async (req, res) => {
-  const {
-    username,
-    password
-  } = req.body;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
   const user = await UserModel.findOne({
-    username
+    username,
   });
   if (!user) {
     return res.json({
-      message: "User does not Exist"
+      message: "User does not Exist",
     });
   }
 
   const hPassword = await bcrypt.compare(password, user.password);
   if (!hPassword) {
     return res.json({
-      message: "Wrong Password"
+      message: "Wrong Password",
     });
   }
 
-
-  const token = jwt.sign({
-    id: user._id
-  }, "predator");
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    "predator"
+  );
   res.json({
     token,
-    userID: user._id
+    userID: user._id,
   });
 });
-export {
-  router as userRouter
-};
-
+export { router as userRouter };
 
 export const verifyToken = (req, res, next) => {
   const token = req.headers.authorization;
@@ -74,7 +64,6 @@ export const verifyToken = (req, res, next) => {
       if (err) return res.sendStatus(403);
       next();
     });
-
   } else {
     res.sendStatus(401);
   }
